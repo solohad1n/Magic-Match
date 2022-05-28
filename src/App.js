b13d99afa1428b12ea4049205241dc83f9ebba99
solohad1n <fox663399@gmail.com>
2022-05-28 21:30:25 +0600

    Added handleChoice function

--- src/App.js
+++ src/App.js
@@ -4,52 +4,61 @@ import Card from './components/Card';
 
 const cardImages = [
   {
-    src: '/img/helmet-1png',
+    src: '/img/helmet-1.png',
+    isMatched: false,
   },
   {
-    src: '/img/potion-1png',
+    src: '/img/potion-1.png',
+    isMatched: false,
   },
   {
-    src: '/img/ring-1png',
+    src: '/img/ring-1.png',
+    isMatched: false,
   },
   {
-    src: '/img/scroll-1png',
+    src: '/img/scroll-1.png',
+    isMatched: false,
   },
   {
-    src: '/img/shield-1png',
+    src: '/img/shield-1.png',
+    isMatched: false,
   },
   {
-    src: '/img/sword-1png',
+    src: '/img/sword-1.png',
+    isMatched: false,
   },
 ]
 
 function App() {
   const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [turns, serTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
-    setCards(shuffledCards)
-    setTurns(0)
-    console.log(shuffledCards)
+    const shuffleCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffleCards)
+    serTurns(0)
+  }
+  const handleChoice = (card) => {
+    if (choiceOne) {
+      setChoiceTwo(card)
+    } else {
+      setChoiceOne(card)
+    }
+
   }
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
-        {cards.map((card) => {
-          return (
-            <div className='card'>
-              <div>
-                {cards.map((card) => <Card key={card.id} card={card} />)}
-              </div>
-            </div>
-          )
-        })}
+        {cards.map((card) => (<Card handleChoice={handleChoice} key={card.id} card={card} />
+        ))}
       </div>
     </div>
-  );
+  )
 }
 
 export default App
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './style.css'
 
-const Card = ({ card }) => {
+const Card = ({ card, handleChoice }) => {
   return (
     <div className='card'>
       <div>
