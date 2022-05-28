34d34a1c0a320934a2e6800f622740dd73b369b2
solohad1n <fox663399@gmail.com>
2022-05-28 17:21:44 +0600

    Added cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import { useState } from 'react';
 import './App.css'
 
-
+const cardImages = [
+  {
+    src: '/img/helmet-1png',
+    isMatched: false,
+  },
+  {
+    src: '/img/potion-1png',
+    isMatched: false,
+  },
+  {
+    src: '/img/ring-1png',
+    isMatched: false,
+  },
+  {
+    src: '/img/scroll-1png',
+    isMatched: false,
+  },
+  {
+    src: '/img/shield-1png',
+    isMatched: false,
+  },
+  {
+    src: '/img/sword-1png',
+    isMatched: false,
+  },
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffledCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffledCards}>New Game</button>
     </div>
   );
 }
